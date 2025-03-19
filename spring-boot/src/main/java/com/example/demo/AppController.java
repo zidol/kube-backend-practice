1306a48d5bec54f9e36866a3fa4f3763e5cfe9bc
zidol <ajh3166@naver.com>
2025-03-20 00:07:29 +0900

    Feat: 환경변수 적용 방법

--- spring-boot/src/main/java/com/example/demo/AppController.java
+++ spring-boot/src/main/java/com/example/demo/AppController.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
 
+    @Value("${MY_ACCOUNT:default}")
+    private String myAccount;
+
+    @Value("${MY_PASSWORD:default}")
+    private String myPassword;
+
     @GetMapping("/")
     public String home() {
         return "Version 1.1";
+    }
+
+    @GetMapping("/value")
+    public String home2() {
+        return "myAccount: " + myAccount + ", myPassword: " + myPassword;
     }
 }
