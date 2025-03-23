315425df651b0c13cf3a82186d50fa1ee9041164
zidol <ajh3166@naver.com>
2025-03-23 17:25:16 +0900

    Feat : spring-boot, mysql 쿠버네티스에서 연동

---
+++ spring-mysql/src/main/java/com/example/spring_mysql/AppController.java
@@ -0,0 +1,13 @@
+package com.example.spring_mysql;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+}
